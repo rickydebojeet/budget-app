5335daea767c85841453967edb3e8c106d74c663
Debojeet Das <debojeetofficial@gmail.com>
2020-09-14 20:26:22 +0000

    Adding __str__() function

--- budget.py
+++ budget.py
@@ -1,11 +1,11 @@
 class Category:
   name = ""
   ledger = list()
-  balance = float(0)
 
   #To instantiate between objects
   def __init__(self, x):
     self.name = x
+    self.balance = 0.0
 
   #Deposit method
   def deposit(self, amount, description = ""):
@@ -14,7 +14,7 @@ class Category:
 
   #Withdraw method
   def withdraw(self, amount, description = ""):
-    if self.check_funds(amount):
+    if self.check_funds(amount) == False:
       return False
     else:
       self.ledger.append({"amount" : (0 - float(amount)), "description" : description})
@@ -23,11 +23,11 @@ class Category:
 
   #Get balance method
   def get_balance(self):
-    return balance
+    return self.balance
 
   #Transfer method
   def transfer(self, amount, cat):
-    if self.check_funds(amount):
+    if self.check_funds(amount) == False:
       return False
     else:
       self.withdraw(amount, ("Transfer to " + cat))
@@ -36,12 +36,28 @@ class Category:
 
   #Check funds method
   def check_funds(self, amount):
-    if float(amount) > balance:
+    if float(amount) > self.balance:
       return False
     else:
       return True
 
+  #Defining return values
+  def __str__(self):
+    displayer = ""
+    fline = self.name.center(30, "*")
+    fline = fline.rstrip()
+    displayer = displayer + fline + '\n'
+    for items in self.ledger:
+      item = items.values()
+      spart = item[0]
+      spart = str('%.2f'%spart)
+      fpart = item[1]
+      fpart = fpart[: 24].ljust(23)
+      spart = spart[: 8].rjust(7)
+      displayer = displayer + fpart + spart + "\n"
+    displayer = displayer + "Total: " + str('%.2f'%self.balance) 
+    return displayer
   
-
+    
 
 def create_spend_chart(categories):
