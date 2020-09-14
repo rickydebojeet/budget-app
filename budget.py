5ad7dc29dc166fdd53e3f325d3e417afbf46ba54
Debojeet Das <debojeetofficial@gmail.com>
2020-09-14 18:02:43 +0000

    added deposit method

--- budget.py
+++ budget.py
@@ -1,11 +1,12 @@
 class Category:
   name = ""
+  ledger = list()
 
   def __init__(self, x):
     self.name = x
 
-
-
+  def deposit(self, amount, description = ""):
+    self.ledger.append({"amount" : float(amount), "description" : description})
 
 
 def create_spend_chart(categories):
