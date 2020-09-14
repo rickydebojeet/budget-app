0bd5817c362720f3f3784bf607f03037d32bfd4c
Debojeet Das <debojeetofficial@gmail.com>
2020-09-14 18:26:01 +0000

    added transfer method

--- budget.py
+++ budget.py
@@ -3,20 +3,36 @@ class Category:
   ledger = list()
   balance = float(0)
 
+  #To instantiate between objects
   def __init__(self, x):
     self.name = x
 
+  #Deposit method
   def deposit(self, amount, description = ""):
     self.ledger.append({"amount" : float(amount), "description" : description})
-    balance = balance + float(amount)
+    self.balance = self.balance + float(amount)
 
+  #Withdraw method
   def withdraw(self, amount, description = ""):
-    if float(amount) > balance:
+    if float(amount) > self.balance:
       return False
     else:
       self.ledger.append({"amount" : (0 - float(amount)), "description" : description})
-      balance = balance - float(amount)
+      self.balance = self.balance - float(amount)
       return True
+
+  #Get balance method
+  def get_balance(self):
+    return balance
+
+  #Transfer method
+  def transfer(self, amount, cat):
+    if float(amount) > balance:
+      return False
+    else:
+      self.withdraw(amount, ("Transfer to " + cat))
+
+  
 
 
 def create_spend_chart(categories):
