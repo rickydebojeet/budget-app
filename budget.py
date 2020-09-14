a2de2de1e395004b4a64c77bc07996d5614fdc4d
Debojeet Das <debojeetofficial@gmail.com>
2020-09-14 18:14:15 +0000

    added withdraw method

--- budget.py
+++ budget.py
@@ -1,12 +1,22 @@
 class Category:
   name = ""
   ledger = list()
+  balance = float(0)
 
   def __init__(self, x):
     self.name = x
 
   def deposit(self, amount, description = ""):
     self.ledger.append({"amount" : float(amount), "description" : description})
+    balance = balance + float(amount)
+
+  def withdraw(self, amount, description = ""):
+    if float(amount) > balance:
+      return False
+    else:
+      self.ledger.append({"amount" : (0 - float(amount)), "description" : description})
+      balance = balance - float(amount)
+      return True
 
 
 def create_spend_chart(categories):
